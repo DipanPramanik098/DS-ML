16c53a91f059a4f5d56d2870240df240d08cc839
DipanPramanik098 <myselfdipan6@gmail.com>
2025-07-24 16:39:59 +0530

    Pyhton

---
+++ _01_Python/_02_practiceQ/_02_areaOfCircle.py
@@ -0,0 +1,5 @@
+import math;
+
+a = int(input("Enter radius : "));
+
+print("area of cirle : ", math.pi * a*a);
---
+++ _01_Python/_02_practiceQ/_04_nameRev.py
@@ -0,0 +1,7 @@
+a = input("Enter Your first Name ");
+b = input("Enter Your last Name ");
+
+
+a= a+" "+b;
+
+print(a[::-1]);
